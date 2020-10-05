de7f117c738083682f5c8e3607faf5e29f8322b8
Collin Smith <collin.smith@iostudio.com>
2020-10-05 00:16:45 -0500

    Remove fontawesome kit id

--- themes/collinscode/library/enqueues.php
+++ themes/collinscode/library/enqueues.php
@@ -13,8 +13,8 @@ function theme_enqueues() {
   wp_deregister_script( 'jquery-migrate' );
   wp_register_script( 'jquery-migrate', '//code.jquery.com/jquery-migrate-3.3.1.min.js', array('jquery'), null, false );
 
-  // Enqueue fontawesome
-  wp_enqueue_script( 'fontawesome', '//kit.fontawesome.com/045ec9758a.js', array(), null, true );
+  // Enqueue fontawesome — be sure to add kit id
+  // wp_enqueue_script( 'fontawesome', '//kit.fontawesome.com/.js', array(), null, true );
 
   // Enqueue lodash
   wp_enqueue_script( 'lodash', '//cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.20/lodash.min.js', array(), null, true );
